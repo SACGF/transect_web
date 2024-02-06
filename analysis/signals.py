b8978607efe96d1c68ed88336844feb425e86b7e
Yasir <yasirsaad1234@hotmail.com>
2024-02-06 13:47:00 +1030

    added more error checks for the celery function, in addition, added a signal that will delete the associated directories when an analysis from the Analysis model gets deleted

--- analysis/signals.py
+++ analysis/signals.py
@@ -1,12 +1,20 @@
-from djano.db.models.signals import pre_delete, post_delete
-from analysis.models import Student
+from django.db.models.signals import pre_delete, post_delete
+from analysis.models import Analysis
 from django.dispatch import receiver
 from sRT_backend.settings import env
 import os
-
+import shutil
 
 # defining a receiver function and what signal it connects to and the sender model
 @receiver(pre_delete, sender=Analysis)
 def delete_associated_directory(sender, instance, **kwargs):
-    analysisOutDir = os.path.join(env('OUTPUT_DIR'), instance.sha_hash + ".zip")
-    os.path.remove(analysisOutDir)
+    analysisOutDir = os.path.join(env('OUTPUT_DIR'), instance.sha_hash)
+    try:
+        shutil.rmtree(analysisOutDir)
+    except:
+        pass
+
+    try:
+        os.remove(analysisOutDir + ".zip")
+    except:
+        pass
--- analysis/tasks.py
+++ analysis/tasks.py
@@ -8,48 +8,75 @@ from analysis.models import Analysis, Genes, Projects
 
 from celery import shared_task
 
+def delete_folder(out_path):
+    try:
+        shutil.rmtree(out_path)
+    except:
+        pass
+
+def delete_file(file_to_delete):
+    try:
+        os.remove(file_to_delete)
+    except:
+        pass
+
 # if composite_analysis_type == "Single" and curr_percentile is not 0, this trigger de_analysis
 @shared_task
 def submit_command(project, gene, composite_analysis_type, percentile, rna_species, sha_hash, outdir, analysis_script_path):
-    command = analysis_script_path + " -p " + project + " -g "
-    # INSPECT! Change gene.split(",") to just gene
-    all_gois = gene.split(",")
-    
-    # adding different settings based on whether de or correlation, 
-    # if correlation, set percentile input and rna_species_to_analyse
-    # adjusted also the analysis script to accomodate this
-    if percentile != 0:
-        to_add = ""
-        if composite_analysis_type == "Single":
-            to_add = all_gois[0]
-        elif composite_analysis_type == "Multi":
-            to_add = "+".join(all_gois)
-        elif composite_analysis_type == "Ratio":
-            to_add = "%".join(all_gois)
+    try:
+        command = analysis_script_path + " -p " + project + " -g "
+        # INSPECT! Change gene.split(",") to just gene
+        all_gois = gene.split(",")
         
-        command += to_add
-        command += " -t " + percentile
-        command += " -s " + rna_species
-        command += " -d "
-    else:
-        command += all_gois[0]
-        command += " -c "
+        # adding different settings based on whether de or correlation, 
+        # if correlation, set percentile input and rna_species_to_analyse
+        # adjusted also the analysis script to accomodate this
+        if percentile != 0:
+            to_add = ""
+            if composite_analysis_type == "Single":
+                to_add = all_gois[0]
+            elif composite_analysis_type == "Multi":
+                to_add = "+".join(all_gois)
+            elif composite_analysis_type == "Ratio":
+                to_add = "%".join(all_gois)
+            
+            command += to_add
+            command += " -t " + percentile
+            command += " -s " + rna_species
+            command += " -d "
+        else:
+            command += all_gois[0]
+            command += " -c "
 
-    out_path = os.path.join(outdir, sha_hash)
-    os.mkdir(out_path)
+        out_path = os.path.join(outdir, sha_hash)
 
-    command += " -o " + out_path
-    # run the command
-    # if the command failed, delete its folder and associated database entry
-    completed_process = subprocess.run(command, shell=True)
-    if completed_process.returncode != 0:
+        # just a sanity check, to see if path already exists
+        # better to just delete and start again it in case there are problems with the old version
+        # also delete the zip file
+        # under normal circumstances, this is unecessary because this script only gets triggered 
+        # when the analysis is not detected in the database
+        delete_folder(out_path)
+        delete_file(out_path + ".zip")
+
+        os.mkdir(out_path)
+
+        command += " -o " + out_path
+        # run the command
+        # if the command failed, delete its folder and associated database entry
+        completed_process = subprocess.run(command, shell=True)
+        if completed_process.returncode != 0:
+            Analysis.objects.filter(sha_hash=sha_hash).first().delete()
+            delete_folder(out_path)
+            delete_file(out_path + ".zip")
+        else:
+            analysis_obj = Analysis.objects.filter(sha_hash=sha_hash).first()
+            analysis_obj.fully_downloaded = True
+            analysis_obj.save()
+            shutil.make_archive(out_path, 'zip', out_path) # finally zip the command
+    except Exception as e:
+        # good catch for the above, if anything happens, we will delete the folders and database entry
+        print(e)
+        out_path = os.path.join(outdir, sha_hash)
+        delete_folder(out_path)
+        delete_file(out_path + ".zip")
         Analysis.objects.filter(sha_hash=sha_hash).first().delete()
-        try:
-            shutil.rmtree(path)
-        except:
-            pass
-    else:
-        analysis_obj = Analysis.objects.filter(sha_hash=sha_hash).first()
-        analysis_obj.fully_downloaded = True
-        analysis_obj.save()
-        shutil.make_archive(out_path, 'zip', out_path) # finally zip the command
