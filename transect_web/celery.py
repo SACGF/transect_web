ede2700ca71d743a5b3e3d22512a30389356f044
Yasir <yasirsaad1234@hotmail.com>
2024-07-15 11:35:27 +0930

    Forgot to add this from last commit

---
+++ transect_web/celery.py
@@ -0,0 +1,26 @@
+import os
+import celery
+from celery import Celery
+from celery.schedules import crontab
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transect_web.settings')
+
+app = Celery('transect_web')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'clean_database_and_analysis': {
+        'task': 'analysis.tasks.clean_database_and_analysis',
+        #'schedule': 900.0,
+        'schedule': crontab(minute=0, hour=3),  # Example: Run at 3am every day
+    },
+}
